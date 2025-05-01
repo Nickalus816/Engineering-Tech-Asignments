2301a287a1036aca0a1cd5a10938425393c03dff
Nickalus816 <lowreynickalus2@gmail.com>
2025-05-01 10:33:51 -0400

    Assignment 1 complete

---
+++ C++/Hello_Test.cpp
@@ -0,0 +1,38 @@
+//Hello Test
+//Nickalus Lowrey
+//Version 1.0.5
+
+// Intructions
+//When testing any of the prompts comment out the others so it doesnt break.
+
+// Orginal Promt
+#include <iostream>
+int main(){
+std::cout << "Hello, World!" << std::endl;
+return 0;
+}
+
+// First Prompt
+#include <iostream>
+int main(){
+std::cout << "Hello, C++!" << std::endl;
+return 0;
+}
+
+// Second Prompt
+#include <iostream>
+int main(){
+std::cout << "Hello, C++!" << std::endl;
+std::cout << std::endl;
+std::cout << "I'm learning to code" << std::endl;
+return 0;
+}
+
+// Third Prompt
+#include <iostream>
+int main(){
+std::cout << "Hello, my name is Nickalus" << std::endl;
+std::cout << std::endl;
+std::cout << "\"AHHHH\" " << std::endl;
+return 0;
+}
---
+++ Hello World/Assignment 1/Hello World with extra/main.cpp
@@ -0,0 +1,8 @@
+// Third Prompt
+#include <iostream>
+int main(){
+std::cout << "Hello, my name is Nickalus" << std::endl;
+std::cout << std::endl;
+std::cout << "\"AHHHH\" " << std::endl;
+return 0;
+}

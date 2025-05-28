76bc1fb0a90a0705c4136ef006d1a93f3d6f5c59
Nickalus816 <lowreynickalus2@gmail.com>
2025-05-28 19:41:14 -0400

    Atrikis Complete

--- C++/Astrakis Pyramid.cpp
+++ C++/Astrakis Pyramid.cpp
@@ -1,11 +1,28 @@
 //Astrakis Pyrimid
 //Nickalus Lowrey
-//Version 0.0.1
+//Version 0.5.1
 
 #include <iostream>
-
-
+using namespace std;
 
 int main(){
-     
+    int rows;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++){
+
+        for (int j = 1; j <= rows - i; j++){
+            cout << " ";
+        }
+
+        for (int k = 1; k <= (2 * i - 1); k++){
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
 }
